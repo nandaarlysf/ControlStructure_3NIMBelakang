b6add78d2f7a97f3ae5f50d4ccac8a55ac7ff837
nandaarlysf <nandaarlys36@gmail.com>
2026-09-24 10:51:38 +0700

    menambahkan bgian a dan b

--- tugas.py
+++ tugas.py
@@ -9,3 +9,13 @@ def evaluate_performance(percentage):
     return "Average performance"
   else:
     return "Needs Improvement (Below Average)"
+
+percentage = float(input("Enter the percentage: "))
+print(evaluate_performance(percentage))
+def find_largest(a, b, c):
+    if a >= b and a >= c:
+        return a
+    elif b >= a and b >= c:
+        return b
+    else:
+        return c
